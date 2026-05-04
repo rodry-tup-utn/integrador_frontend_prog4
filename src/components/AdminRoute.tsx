648711930ef55edf7bc3e8275559ba3d72117f13
rodry <rodrymza@gmail.com>
2026-05-04 12:05:31 -0300

    feat: componente para proteger rutas de administrador

---
+++ src/components/AdminRoute.tsx
@@ -0,0 +1,34 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../modules/auth/context/AuthContext";
+import { toast } from "sonner";
+import { useEffect, useRef } from "react";
+
+export const AdminRoute = () => {
+  const { user, isAuthenticated, isLoading } = useAuth();
+  const hasNotified = useRef(false);
+
+  useEffect(() => {
+    if (
+      !isLoading &&
+      isAuthenticated &&
+      user?.role !== "ADMIN" &&
+      !hasNotified.current
+    ) {
+      toast.error("No tienes permisos de administrador para acceder aquí");
+      hasNotified.current = true;
+    }
+  }, [isLoading, isAuthenticated, user]);
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Verificando permisos...
+      </div>
+    );
+
+  if (!isAuthenticated || user?.role !== "ADMIN") {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  return <Outlet />;
+};
