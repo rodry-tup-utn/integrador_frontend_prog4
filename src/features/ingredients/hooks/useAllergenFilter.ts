b8eb6a68e6e0cb6ec2c571ba75c65fa3ad363a10
rodry <rodrymza@gmail.com>
2026-05-09 00:35:45 -0300

    refactor: logica de filtrado movida a un custom hook

---
+++ src/features/ingredients/hooks/useAllergenFilter.ts
@@ -0,0 +1,37 @@
+import { useMemo, useState } from "react";
+import type { AllergenFilterType } from "../components/AllergenFilterButtons";
+
+type AllergenFilterable = {
+  is_allergen: boolean;
+};
+
+export const useAllergenFilter = <T extends AllergenFilterable>(
+  ingredients: T[],
+) => {
+  const [filterAllergen, setFilterAllergen] =
+    useState<AllergenFilterType>("all");
+
+  const filteredIngredients = useMemo(() => {
+    return ingredients.filter((ing) => {
+      switch (filterAllergen) {
+        case "all":
+          return true;
+
+        case "allergen":
+          return ing.is_allergen;
+
+        case "safe":
+          return !ing.is_allergen;
+
+        default:
+          return true;
+      }
+    });
+  }, [ingredients, filterAllergen]);
+
+  return {
+    filterAllergen,
+    filteredIngredients: filteredIngredients,
+    setFilterAllergen,
+  };
+};
