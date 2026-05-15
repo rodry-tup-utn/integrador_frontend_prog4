c3e4927a50c55b1a5d565b04ac60f7525f408907
rodry <rodrymza@gmail.com>
2026-05-15 11:08:28 -0300

    feat: implementacion de logica de comunicacion con endpoint de categorias: service y hooks

---
+++ src/features/categories/hooks/usePublicCategoryDetail.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { categoryService } from "../services/categoryServices";
+import { categoryKeys } from "../types/category";
+
+export const usePublicCategoryDetail = (id: string | null) => {
+  return useQuery({
+    queryKey: categoryKeys.publicDetail(String(id)),
+    queryFn: () => {
+      if (!id)
+        throw new Error(
+          "Es necesario un id para acceder a los detalles de la categoria",
+        );
+      categoryService.public.getById(id);
+    },
+    enabled: !!id,
+  });
+};
---
+++ src/features/categories/services/categoryServices.ts
@@ -0,0 +1,93 @@
+import api from "../../../shared/api/axiosConfig";
+import type {
+  CategoryCreate,
+  CategoryList,
+  CategoryListPrivate,
+  CategoryPrivate,
+  CategoryPublic,
+  CategoryUpdate,
+} from "../types/category";
+
+const ADMIN_URL = "/admin/category";
+const PUBLIC_URL = "/category";
+export const categoryService = {
+  public: {
+    getAll: async (offset = 0, limit = 20): Promise<CategoryList> => {
+      const response = await api.get<CategoryList>(`${PUBLIC_URL}`, {
+        params: { offset, limit },
+      });
+      return response.data;
+    },
+
+    getById: async (id: string): Promise<CategoryPublic> => {
+      const response = await api.get<CategoryPublic>(`${PUBLIC_URL}/${id}`);
+      return response.data;
+    },
+
+    search: async (
+      query: string,
+      offset = 0,
+      limit = 20,
+    ): Promise<CategoryList> => {
+      const response = await api.get<CategoryList>(`${PUBLIC_URL}/search`, {
+        params: { query, offset, limit },
+      });
+      return response.data;
+    },
+  },
+
+  admin: {
+    getAll: async (offset = 0, limit = 20): Promise<CategoryListPrivate> => {
+      const response = await api.get<CategoryListPrivate>(`${ADMIN_URL}`, {
+        params: { offset, limit },
+      });
+      return response.data;
+    },
+
+    getById: async (id: string): Promise<CategoryPrivate> => {
+      const response = await api.get<CategoryPrivate>(`${ADMIN_URL}/${id}`);
+      return response.data;
+    },
+
+    create: async (data: CategoryCreate): Promise<CategoryPrivate> => {
+      const response = await api.post<CategoryPrivate>(`${ADMIN_URL}`, data);
+      return response.data;
+    },
+
+    update: async (
+      id: number,
+      data: CategoryUpdate,
+    ): Promise<CategoryPrivate> => {
+      const response = await api.patch<CategoryPrivate>(
+        `${ADMIN_URL}/${id}`,
+        data,
+      );
+      return response.data;
+    },
+
+    delete: async (id: number): Promise<void> => {
+      await api.delete(`${ADMIN_URL}/${id}`);
+    },
+
+    restore: async (id: number): Promise<CategoryPrivate> => {
+      const response = await api.patch<CategoryPrivate>(
+        `${ADMIN_URL}/${id}/restore`,
+      );
+      return response.data;
+    },
+
+    search: async (
+      query: string,
+      offset = 0,
+      limit = 20,
+    ): Promise<CategoryListPrivate> => {
+      const response = await api.get<CategoryListPrivate>(
+        `${ADMIN_URL}/search`,
+        {
+          params: { query, offset, limit },
+        },
+      );
+      return response.data;
+    },
+  },
+};
