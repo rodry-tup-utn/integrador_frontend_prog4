30d5f21477af7d8f8cc06b5fbaa602cab7563ac5
rodry <rodrymza@gmail.com>
2026-05-18 03:41:35 -0300

    feat: configuracion de mantine

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./features/auth/context/AuthContext.tsx";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./shared/theme/theme.ts";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <MantineProvider theme={theme}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </MantineProvider>
   </StrictMode>,
 );
