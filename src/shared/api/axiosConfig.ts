a019a2acdc9dec4fda1f0454e92837b7e60fc7cd
rodry <rodrymza@gmail.com>
2026-05-24 18:06:13 -0300

    feat: agregado interceptor para agregar timezone a respuestas de datetime

--- src/shared/api/axiosConfig.ts
+++ src/shared/api/axiosConfig.ts
@@ -9,4 +9,18 @@ const api: AxiosInstance = axios.create({
   withCredentials: true,
 });
 
+// interceptor para formatear todas las fechas a UTC para evitar errores de zona horaria
+api.interceptors.response.use((response) => {
+  response.data = JSON.parse(
+    JSON.stringify(response.data).replace(
+      /"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.*?)"/g,
+      (match) =>
+        match.endsWith('Z"') || match.includes("+")
+          ? match
+          : match.slice(0, -1) + 'Z"',
+    ),
+  );
+  return response;
+});
+
 export default api;
