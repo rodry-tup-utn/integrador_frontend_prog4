eea7ff3a4f2634776018cc61386588aaf4ee6aaa
rodry <rodrymza@gmail.com>
2026-06-03 14:05:23 -0300

    feat: mejora de visualizacion

--- src/features/products/pages/ProductsAdminPage.tsx
+++ src/features/products/pages/ProductsAdminPage.tsx
@@ -1,10 +1,21 @@
-import { Button, Group, Pagination, Text, TextInput, Title } from "@mantine/core"
+import {
+  Button,
+  Group,
+  Pagination,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
 import { IconPlus, IconSearch } from "@tabler/icons-react";
 import { useState } from "react";
 import { useDebouncedValue } from "@mantine/hooks";
 import ProductsTable from "../components/ProductsTable";
 import { useAdminProducts } from "../hooks/product.queries.hooks";
-import { type ProductCreate, type ProductFilters, type ProductPrivate } from "../types/product";
+import {
+  type ProductCreate,
+  type ProductFilters,
+  type ProductPrivate,
+} from "../types/product";
 import ProductsModal from "../components/ProductsModal";
 import ProductsForm from "../components/ProductsForm";
 import { useProductMutation } from "../hooks/product.mutation.hooks";
@@ -12,30 +23,32 @@ import { notifications } from "@mantine/notifications";
 import type { AxiosError } from "axios";
 
 const ProductsAdminPage = () => {
+  const LIMIT = 20;
 
-  const LIMIT = 20
-
-  const [page, setPage] = useState(1)
-  const [searchTerm, setSearchTerm] = useState("")
-  const [debouncedSearch] = useDebouncedValue(searchTerm, 300)
-  const [editing, setEditing] = useState<ProductPrivate | null>(null)
-  const [open, setOpen] = useState(false)
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearch] = useDebouncedValue(searchTerm, 300);
+  const [editing, setEditing] = useState<ProductPrivate | null>(null);
+  const [open, setOpen] = useState(false);
 
   const filters: ProductFilters = {
     offset: (page - 1) * LIMIT,
     limit: LIMIT,
     search: debouncedSearch || undefined,
-  }
+  };
 
-  const { data, isLoading } = useAdminProducts(filters)
-  const { createProduct, updateProduct, deleteProduct, restoreProduct } = useProductMutation()
+  const { data, isLoading } = useAdminProducts(filters);
+  const { createProduct, updateProduct, deleteProduct, restoreProduct } =
+    useProductMutation();
 
-  const totalPages = data ? Math.ceil(data.total / (filters.limit ?? LIMIT)) : 0
+  const totalPages = data
+    ? Math.ceil(data.total / (filters.limit ?? LIMIT))
+    : 0;
 
   const handleEdit = (item: ProductPrivate) => {
-    setEditing(item)
-    setOpen(true)
-  }
+    setEditing(item);
+    setOpen(true);
+  };
 
   const handleDelete = (id: number) => {
     notifications.show({
@@ -43,26 +56,24 @@ const ProductsAdminPage = () => {
       message: (
         <Group justify="space-between" wrap="nowrap">
           <Text size="md">{`¿Eliminar el producto #${id}?`}</Text>
-          <Button onClick={() => executeDelete(id)}>
-            Eliminar
-          </Button>
+          <Button onClick={() => executeDelete(id)}>Eliminar</Button>
         </Group>
-      )
-    })
-  }
+      ),
+    });
+  };
 
   const executeDelete = (id: number) => {
     deleteProduct(id, {
       onSuccess: () => {
-        notifications.show({ color: "green", message: "Producto eliminado" })
+        notifications.show({ color: "green", message: "Producto eliminado" });
       },
       onError: (error) => {
-        const axiosError = error as AxiosError<{ detail: string }>
-        const msg = axiosError.response?.data?.detail || "Error al eliminar"
-        notifications.show({ color: "red", message: msg })
-      }
-    })
-  }
+        const axiosError = error as AxiosError<{ detail: string }>;
+        const msg = axiosError.response?.data?.detail || "Error al eliminar";
+        notifications.show({ color: "red", message: msg });
+      },
+    });
+  };
 
   const handleRestore = (id: number) => {
     notifications.show({
@@ -70,70 +81,78 @@ const ProductsAdminPage = () => {
       message: (
         <Group justify="space-between" wrap="nowrap">
           <Text size="md">{`¿Restaurar el producto #${id}?`}</Text>
-          <Button onClick={() => executeRestore(id)}>
-            Restarar
-          </Button>
+          <Button onClick={() => executeRestore(id)}>Restaurar</Button>
         </Group>
-      )
-    })
-  }
+      ),
+    });
+  };
 
   const executeRestore = (id: number) => {
     restoreProduct(id, {
       onSuccess: () => {
-        notifications.show({ color: "green", message: "Producto restaurado" })
+        notifications.show({ color: "green", message: "Producto restaurado" });
       },
       onError: (error) => {
-        const axiosError = error as AxiosError<{ detail: string }>
-        const msg = axiosError.response?.data?.detail || "Error al restaurar"
-        notifications.show({ color: "red", message: msg })
-      }
-    })
-  }
+        const axiosError = error as AxiosError<{ detail: string }>;
+        const msg = axiosError.response?.data?.detail || "Error al restaurar";
+        notifications.show({ color: "red", message: msg });
+      },
+    });
+  };
 
   const handleClose = () => {
-    setEditing(null)
-    setOpen(false)
-  }
+    setEditing(null);
+    setOpen(false);
+  };
 
   const handleSubmit = (data: ProductCreate) => {
     if (editing) {
-      updateProduct({ id: editing!.id, data }, {
-        onSuccess: () => {
-          setOpen(false)
-          setEditing(null)
-          notifications.show({ color: "green", message: "Producto actualizado" })
+      updateProduct(
+        { id: editing!.id, data },
+        {
+          onSuccess: () => {
+            setOpen(false);
+            setEditing(null);
+            notifications.show({
+              color: "green",
+              message: "Producto actualizado",
+            });
+          },
+          onError: (error) => {
+            const axiosError = error as AxiosError<{ detail: string }>;
+            const msg =
+              axiosError.response?.data?.detail || "Error al actualizar";
+            notifications.show({ color: "red", message: msg });
+          },
         },
-        onError: (error) => {
-          const axiosError = error as AxiosError<{ detail: string }>
-          const msg = axiosError.response?.data?.detail || "Error al actualizar"
-          notifications.show({ color: "red", message: msg })
-        }
-      })
+      );
     } else {
       createProduct(data, {
         onSuccess: () => {
-          setOpen(false)
-          setEditing(null)
-          notifications.show({ color: "green", message: "Producto creado" })
+          setOpen(false);
+          setEditing(null);
+          notifications.show({ color: "green", message: "Producto creado" });
         },
         onError: (error) => {
-          const axiosError = error as AxiosError<{ detail: string }>
-          const msg = axiosError.response?.data?.detail || "Error al crear el producto"
-          notifications.show({ color: "red", message: msg })
-        }
-      })
+          const axiosError = error as AxiosError<{ detail: string }>;
+          const msg =
+            axiosError.response?.data?.detail || "Error al crear el producto";
+          notifications.show({ color: "red", message: msg });
+        },
+      });
     }
-  }
+  };
 
   return (
     <div className="flex flex-col">
-      <Title order={2} style={{ marginBottom: "8px" }}>Productos</Title>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4" >
-        <section className="cell p-2" >
+      <Title order={2} style={{ marginBottom: "8px" }}>
+        Productos
+      </Title>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <section className="cell p-2">
           <div className="flex items-center justify-center md:justify-start">
             <TextInput
-              placeholder="Buscar categoría..."
+              placeholder="Buscar producto..."
               leftSection={<IconSearch size={16} />}
               value={searchTerm}
               onChange={(evt) => {
@@ -145,13 +164,13 @@ const ProductsAdminPage = () => {
           </div>
         </section>
 
-        <section className="cell py-2 px-4" >
+        <section className="cell py-2 px-4">
           <div className="flex items-center justify-center md:justify-end">
             <Button
               leftSection={<IconPlus size={16} />}
               onClick={() => {
-                console.log("Nuevo producto")
-                setOpen(true)
+                console.log("Nuevo producto");
+                setOpen(true);
               }}
             >
               Nuevo Producto
@@ -161,7 +180,14 @@ const ProductsAdminPage = () => {
 
         <section className="cell md:col-span-2">
           <div className="flex flex-col items-start justify-between min-h-50 p-2 shadow-md border-b-gray-400 rounded-md">
-            <ProductsTable isLoading={isLoading} data={data} onEdit={handleEdit} onModalOpen={setOpen} onDelete={handleDelete} onRestore={handleRestore} />
+            <ProductsTable
+              isLoading={isLoading}
+              data={data}
+              onEdit={handleEdit}
+              onModalOpen={setOpen}
+              onDelete={handleDelete}
+              onRestore={handleRestore}
+            />
           </div>
         </section>
 
@@ -175,21 +201,24 @@ const ProductsAdminPage = () => {
 
         <section className="cell p-2" style={{ border: "2px doted green" }}>
           <div className="flex items-center justify-center md:justify-end">
-            <Pagination total={totalPages || 1} value={page} onChange={setPage} />
+            <Pagination
+              total={totalPages || 1}
+              value={page}
+              onChange={setPage}
+            />
           </div>
         </section>
-
       </div>
       <ProductsModal open={open} handleClose={handleClose}>
         <ProductsForm
           handleClose={handleClose}
           editing={editing}
-          key={editing?.id ?? 'new'}
+          key={editing?.id ?? "new"}
           onSubmit={handleSubmit}
         />
       </ProductsModal>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsAdminPage
+export default ProductsAdminPage;
