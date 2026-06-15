566eb31284b1b9597c4657752aec232d2cde8059
Leandro-Mercado-Simi <leandro.mercado@alumnos.frm.utn.edu.ar>
2026-06-15 19:04:40 -0300

    fix: corrección de import roto después del merge

--- src/features/user/components/admin/AdminUserCreateModal.tsx
+++ src/features/user/components/admin/AdminUserCreateModal.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { extractApiErrorMessage } from "../../../shared/helpers/apiErrors";
 import { notifications } from "@mantine/notifications";
 import {
   Modal,
@@ -13,6 +12,7 @@ import {
 import { type UserCreateByAdmin, type RoleCodes } from "../../types/user";
 import { useAdminUserMutations } from "../../hooks/admin/useAdminUserMutations";
 import { IconPlus } from "@tabler/icons-react";
+import { extractApiErrorMessage } from "../../../../shared/helpers/apiErrors";
 
 interface Props {
   opened: boolean;
