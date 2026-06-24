93bbeec4e02b12df0b52b57d325c780fac1d8c60
rodry <rodrymza@gmail.com>
2026-06-23 21:32:53 -0300

    refactor: columnas ocultadas en mobile,

--- src/features/products/components/ProductsTable.tsx
+++ src/features/products/components/ProductsTable.tsx
@@ -9,6 +9,7 @@ import {
   IconInfoCircle,
   IconRestore,
   IconTrash,
+  IconEyeSearch,
 } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../auth/context/AuthContext";
@@ -24,6 +25,7 @@ interface ProductsTableProps {
   onRestore: (item: ProductPrivate) => void;
   onDelete: (item: ProductPrivate) => void;
   onModalOpen: (value: boolean) => void;
+  onEdit: (id: number) => void;
 }
 
 const mapType = (type: TypeProduct) => {
@@ -38,6 +40,7 @@ const ProductsTable = ({
   data,
   onRestore,
   onDelete,
+  onEdit,
 }: ProductsTableProps) => {
   const { user } = useAuth();
   const { data: measurementUnits } = useMeasurementUnits();
@@ -80,164 +83,153 @@ const ProductsTable = ({
   };
 
   return (
-    <Table.ScrollContainer minWidth={700}>
-      <Table striped="odd" stripedColor="#fff" highlightOnHover>
-        <Table.Thead>
+    <Table striped="odd" stripedColor="#fff" highlightOnHover>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>ID</Table.Th>
+          <Table.Th>Nombre</Table.Th>
+          <Table.Th>Precio</Table.Th>
+          <Table.Th>Estado</Table.Th>
+          <Table.Th ta="center">Tipo</Table.Th>
+          <Table.Th>
+            <Group gap={4}>
+              Stock
+              <Tooltip label="Stock de manufacturados calculados en base al stock de ingredientes disponible">
+                <IconInfoCircle size={16} style={{ cursor: "pointer" }} />
+              </Tooltip>
+            </Group>
+          </Table.Th>
+          <Table.Th ta="center">Unidad</Table.Th>
+          <Table.Th ta="center">Disponibilidad</Table.Th>
+          <Table.Th style={{ textAlign: "center" }}>Acciones</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>
+        {isLoading ? (
           <Table.Tr>
-            <Table.Th>ID</Table.Th>
-            <Table.Th>Nombre</Table.Th>
-            <Table.Th>Precio</Table.Th>
-            <Table.Th>Estado</Table.Th>
-            <Table.Th ta="center">Tipo</Table.Th>
-            <Table.Th>
-              <Group gap={4}>
-                Stock
-                <Tooltip label="Stock de manufacturados calculados en base al stock de ingredientes disponible">
-                  <IconInfoCircle size={16} style={{ cursor: "pointer" }} />
-                </Tooltip>
-              </Group>
-            </Table.Th>
-            <Table.Th ta="center" hiddenFrom="sm">
-              Unidad
-            </Table.Th>
-            <Table.Th ta="center" hiddenFrom="sm">
-              Disponibilidad
-            </Table.Th>
-            <Table.Th style={{ textAlign: "center" }}>Acciones</Table.Th>
+            <Table.Td colSpan={8}>
+              <Text ta="center" py="xl">
+                Cargando...
+              </Text>
+            </Table.Td>
           </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {isLoading ? (
-            <Table.Tr>
-              <Table.Td colSpan={9}>
-                <Text ta="center" py="xl">
-                  Cargando...
-                </Text>
-              </Table.Td>
-            </Table.Tr>
-          ) : !data?.data.length ? (
-            <Table.Tr>
-              <Table.Td colSpan={9}>
-                <Text ta="center" py="xl">
-                  No hay productos.
-                </Text>
-              </Table.Td>
-            </Table.Tr>
-          ) : (
-            data.data.map((item) => {
-              const isDeleted = !!item?.deleted_at;
-              return (
-                <Table.Tr key={item.id} opacity={isDeleted ? 0.6 : undefined}>
-                  <Table.Td>
-                    <Text size="sm" c="dimmed">
-                      #{item.id}
-                    </Text>
-                  </Table.Td>
-                  <Table.Td>
-                    <Text td={isDeleted ? "line-through" : undefined} fw={500}>
-                      {item.name}
-                    </Text>
-                  </Table.Td>
-                  <Table.Td>
-                    <Text size="sm" c="dark" lineClamp={1}>
-                      {`$ ${item.base_price}`}
-                    </Text>
-                  </Table.Td>
-                  <Table.Td>
-                    <Badge
-                      color={isDeleted ? "red" : "teal"}
-                      variant="filled"
-                      size="sm"
-                    >
-                      {isDeleted ? "Inactivo" : "Activo"}
-                    </Badge>
-                  </Table.Td>
-                  <Table.Td ta="center">
-                    <Badge
-                      color={item.type == "FINAL" ? "orange" : "lime"}
-                      variant="light"
-                      size="md"
-                    >
-                      {mapType(item.type)}
-                    </Badge>
-                  </Table.Td>
+        ) : !data?.data.length ? (
+          <Table.Tr>
+            <Table.Td colSpan={9}>
+              <Text ta="center" py="xl">
+                No hay productos.
+              </Text>
+            </Table.Td>
+          </Table.Tr>
+        ) : (
+          data.data.map((item) => {
+            const isDeleted = !!item?.deleted_at;
+            return (
+              <Table.Tr key={item.id} opacity={isDeleted ? 0.6 : undefined}>
+                <Table.Td>
+                  <Text size="sm" c="dimmed">
+                    #{item.id}
+                  </Text>
+                </Table.Td>
+                <Table.Td>
+                  <Text td={isDeleted ? "line-through" : undefined} fw={500}>
+                    {item.name}
+                  </Text>
+                </Table.Td>
+                <Table.Td>
+                  <Text size="sm" c="dark" lineClamp={1}>
+                    {`$ ${item.base_price}`}
+                  </Text>
+                </Table.Td>
+                <Table.Td>
+                  <Badge
+                    color={isDeleted ? "red" : "teal"}
+                    variant="filled"
+                    size="sm"
+                  >
+                    {isDeleted ? "Inactivo" : "Activo"}
+                  </Badge>
+                </Table.Td>
+                <Table.Td ta="center">
+                  <Badge
+                    color={item.type == "FINAL" ? "orange" : "lime"}
+                    variant="light"
+                    size="md"
+                  >
+                    {mapType(item.type)}
+                  </Badge>
+                </Table.Td>
 
-                  <Table.Td>
-                    <Text
-                      size="sm"
-                      c={item.stock > 0 ? "teal" : "red"}
-                      lineClamp={1}
+                <Table.Td>
+                  <Text
+                    size="sm"
+                    c={item.stock > 0 ? "teal" : "red"}
+                    lineClamp={1}
+                  >
+                    {item.type === "MANUFACTURED"
+                      ? `~${item.stock ?? "—"}`
+                      : (item.stock ?? "—")}
+                  </Text>
+                </Table.Td>
+                <Table.Td ta="center">
+                  <Badge size="sm" color="blue" variant="outline">
+                    {resolveSymbol(item.sales_unit || "UNIT")}
+                  </Badge>
+                </Table.Td>
+                <Table.Td ta="center">
+                  <Tooltip
+                    label={
+                      item.available
+                        ? "Marcar No disponible"
+                        : "Marcar Disponible"
+                    }
+                    color={item.available ? "orange" : "teal"}
+                  >
+                    <Button
+                      size="compact-xs"
+                      variant="outline"
+                      color={item.available ? "green" : "red"}
+                      onClick={() => handleAvailability(item, !item.available)}
                     >
-                      {item.type === "MANUFACTURED"
-                        ? `~${item.stock ?? "—"}`
-                        : (item.stock ?? "—")}
-                    </Text>
-                  </Table.Td>
-                  <Table.Td ta="center" hiddenFrom="sm">
-                    <Text
-                      component="span"
-                      c="blue"
-                      fw={600}
-                      size="sm"
-                      bg="blue.0"
-                      px={8}
-                      py={2}
-                      style={{ borderRadius: "var(--mantine-radius-sm)" }}
-                    >
-                      {resolveSymbol(item.sales_unit || "UNIT")}
-                    </Text>
-                  </Table.Td>
-                  <Table.Td ta="center" hiddenFrom="sm">
-                    <Tooltip
-                      label={
-                        item.available
-                          ? "Marcar No disponible"
-                          : "Marcar Disponible"
+                      {item.available ? "Disponible " : "No disponible"}
+                    </Button>
+                  </Tooltip>
+                </Table.Td>
+
+                <Table.Td>
+                  <Group gap={4} justify="center">
+                    <ActionButton
+                      icon={IconEdit}
+                      label="Editar"
+                      color="cyan"
+                      onClick={() => onEdit(item.id)}
+                    />
+                    <ActionButton
+                      icon={IconEyeSearch}
+                      label="Detalles"
+                      color="teal"
+                      onClick={() =>
+                        navigate(`/admin/products/detail/${item.id}`)
                       }
-                      color={item.available ? "orange" : "teal"}
-                    >
-                      <Button
-                        size="compact-xs"
-                        variant="outline"
-                        color={item.available ? "green" : "red"}
-                        onClick={() =>
-                          handleAvailability(item, !item.available)
-                        }
-                      >
-                        {item.available ? "Disponible " : "No disponible"}
-                      </Button>
-                    </Tooltip>
-                  </Table.Td>
-
-                  <Table.Td>
-                    <Group gap={4} justify="center">
+                    />
+                    {isAdmin && (
                       <ActionButton
-                        icon={IconEdit}
-                        label="Editar"
-                        color="teal"
+                        icon={isDeleted ? IconRestore : IconTrash}
                         onClick={() =>
-                          navigate(`/admin/products/detail/${item.id}`)
+                          isDeleted ? onRestore(item) : onDelete(item)
                         }
+                        label={isDeleted ? "Restaurar" : "Eliminar"}
+                        color={isDeleted ? "green" : "red"}
                       />
-                      {isAdmin && (
-                        <ActionButton
-                          icon={isDeleted ? IconRestore : IconTrash}
-                          onClick={() =>
-                            isDeleted ? onRestore(item) : onDelete(item)
-                          }
-                          label={isDeleted ? "Restaurar" : "Eliminar"}
-                          color={isDeleted ? "green" : "red"}
-                        />
-                      )}
-                    </Group>
-                  </Table.Td>
-                </Table.Tr>
-              );
-            })
-          )}
-        </Table.Tbody>
-      </Table>
-    </Table.ScrollContainer>
+                    )}
+                  </Group>
+                </Table.Td>
+              </Table.Tr>
+            );
+          })
+        )}
+      </Table.Tbody>
+    </Table>
   );
 };
 
