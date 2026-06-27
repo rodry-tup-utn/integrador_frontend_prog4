b358bcfcd4282e815d15390fc13bbf3acca345a3
rodry <rodrymza@gmail.com>
2026-06-26 22:37:00 -0300

    feat: useffect para redirigir si no esta autenticado

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { AppShell, Container, Overlay } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import Header from "../../widgets/header/Header";
 import Sidebar from "../../widgets/sidebar/Sidebar";
 import { useAuth } from "../../features/auth/context/AuthContext";
+import { useEffect, useRef } from "react";
 const MainLayout = () => {
   const { isAuthenticated } = useAuth();
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure(false);
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+  const navigate = useNavigate();
+  const prevAuth = useRef(isAuthenticated);
+
+  useEffect(() => {
+    if (prevAuth.current && !isAuthenticated) {
+      navigate("/", { replace: true });
+    }
+    prevAuth.current = isAuthenticated;
+  }, [isAuthenticated, navigate]);
   return (
     <AppShell
       header={{ height: 64 }}
